7b9527f3be1fa266a6b444b981ab0deb3d8d4c0c
reynaldo-dev <reynaldo23.dev@gmail.com>
2023-11-03 23:31:12 -0600

    feat(setup): setup of primeng, primeflex, basic modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrimengModule } from './primeng/primeng.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+     declarations: [AppComponent],
+     imports: [BrowserModule, PrimengModule],
+     providers: [],
+     bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/dashboard/sponsor/sponsor.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SponsorRoutingModule } from './sponsor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SponsorRoutingModule
+  ]
+})
+export class SponsorModule { }
